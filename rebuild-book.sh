c8c07b2278b458a1600d6081f1d72b56ac99fbdb
Emmanuel-R8 <emmanuel.rialland@gmail.com>
2019-11-11 18:35:13 +0800

    Update

---
+++ rebuild-book.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/nice -n 19 env Rscript
+
+rm(list = ls(all.names = TRUE))
+bookdown::render_book("00-index.Rmd", output_format = "bookdown::pdf_book", output_dir = `"documents/pdf")
+bookdown::render_book("00-index.Rmd", output_format = "bookdown::gitbook", output_dir = `"documents/gitbook")
+
+
+
--- rebuild-pdf.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-nice -n 19 R --no-restore --no-save -e 'rm(list = ls(all.names = TRUE)); bookdown::render_book("00-index.Rmd", output_format = "bookdown::pdf_book")'
-
-
-
